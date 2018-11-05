9caae176315d3b3e5ff7974ce648dd37d533acc4
Tatiana <tatiana520@163.com>
2018-11-05 19:39:52 +0800

    :wrench:修改用户列表表单

--- src/components/user-list/user-list.vue
+++ src/components/user-list/user-list.vue
@@ -23,12 +23,22 @@
         </el-button>
       </el-col>
     </el-row>
-    <el-table :data="tableData" style=" width: 100%">
-      <el-table-column prop="username" label="姓名" width="180">
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="username"
+        label="姓名"
+        width="180">
       </el-table-column>
-      <el-table-column prop="email" label="邮箱" width="180">
+      <el-table-column
+        prop="email"
+        label="邮箱"
+        width="180">
       </el-table-column>
-      <el-table-column prop="mobile" label="电话" width="180">
+      <el-table-column
+        prop="mobile"
+        label="电话">
       </el-table-column>
     </el-table>
     <el-pagination
