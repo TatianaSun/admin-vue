19142a82a5d0c3d8414170572afce918378dc05e
Tatiana <tatiana520@163.com>
2018-11-05 11:10:43 +0800

    :robot:添加公司logo&登陆成功存储token&使用导航守卫拦截非登录组件

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,6 +1,12 @@
 <template>
   <el-container class="container">
-    <el-header class="hl-header">Header</el-header>
+    <el-header class="hl-header">
+      <el-row :gutter="20">
+        <el-col :span="4"><div class="grid-content bg-purple"><img style="vertical-align:middle;" src="./logo.png" alt=""></div></el-col>
+        <el-col :span="16"><div class="grid-content bg-purple">好利CMS电商后台管理系统</div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple"><a href="#">退出</a></div></el-col>
+      </el-row>
+    </el-header>
     <el-container class="container">
       <el-aside class="hl-aside" width="200px">Aside</el-aside>
       <el-main class="hl-main">Main</el-main>
@@ -22,6 +28,8 @@ export default {
 }
 .hl-header {
   background-color: #B3C0D1;
+  line-height: 60px;
+  text-align: center;
 }
 .hl-aside {
   background-color: #D3DCE6;
--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import Home from '@/components/home/home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       name: 'login',
@@ -19,3 +19,25 @@ export default new Router({
     }
   ]
 })
+
+// 添加路由导航守卫
+router.beforeEach((to, from, next) => {
+  // 如果是登录组件,则直接放行通过
+  // 如果不是登录组件,则检查token令牌,有令牌就放行,没有令牌就让其跳转到登录页
+  // console.log(to)
+  if (to.name === 'login') {
+    next()
+  } else {
+    // 检查token令牌,有则放心,无则跳转登录组件
+    const token = window.localStorage.getItem('admin-token')
+    if (!token) {
+      next({
+        name: 'login'
+      })
+    } else {
+      next()
+    }
+  }
+})
+
+export default router
