c3c47e3c7ad6b062c1be037097a0aa73717f75fc
Tatiana <tatiana520@163.com>
2018-11-15 17:39:37 +0800

    :pushpin:完成带有表单验证的添加用户功能

--- src/components/role-list/role-list.vue
+++ src/components/role-list/role-list.vue
@@ -1,7 +1,33 @@
 <template>
-<div>角色列表</div>
+  <div>
+    <el-dropdown>
+      <span class="el-dropdown-link">
+        高级筛选<i class="el-icon-arrow-down el-icon--right"></i>
+      </span>
+      <el-dropdown-menu slot="dropdown" class="choose">
+        <div style="border-left: 5px solid red; padding-left: 8px;">
+          情绪选择
+        </div>
+        <div>
+          <span><a href="#">全部情绪</a></span>
+          <span><a href="#">正面</a></span>
+          <span><a href="#">中性</a></span>
+          <span><a href="#">负面</a></span>
+        </div>
+        <div style="border-left: 5px solid red; padding-left: 8px;">
+          排序
+        </div>
+        <div>
+          <span><a href="#">默认</a></span>
+          <span><a href="#">相关性</a></span>
+          <span><a href="#">热度最高</a></span>
+          <span><a href="#">时间升序</a></span>
+          <span><a href="#">时间降序</a></span>
+        </div>
+      </el-dropdown-menu>
+    </el-dropdown>
+  </div>
 </template>
-
 <script>
 export default {
   data () {
@@ -9,6 +35,13 @@ export default {
   }
 }
 </script>
-
 <style>
+.choose {
+  width: 500px;
+  height: 400px;
+  background-color: lightgreen;
+}
+a {
+  text-decoration: none;
+}
 </style>
--- src/components/user-list/user-list.vue
+++ src/components/user-list/user-list.vue
@@ -19,9 +19,10 @@
         </el-input>
       </el-col>
       <el-col :span="2">
-        <el-button type="success" plain>
-          添加用户
-        </el-button>
+        <el-button
+          type="success"
+          plain
+          @click="dialogFormVisible = true">添加用户</el-button>
       </el-col>
     </el-row>
     <el-table
@@ -84,6 +85,41 @@
       layout="total, sizes, prev, pager, next, jumper"
       :total="totalSize">
     </el-pagination>
+    <!-- 添加用户的对话框 -->
+    <el-dialog
+      title="添加用户"
+      :visible.sync="dialogFormVisible">
+      <!-- 为el-form添加表单验证:
+        必须要有:rules: 一个对象
+        表单中el-form-item 要有prop 方法
+
+        如果想要使用javascript来强行验证,需要添加ref属性,给ref起个名字(类似给元素起id名)
+        在javaScript中使用this.$refs['ref名字'].validate()验证表单
+        validate()里面接收一个函数,一般用箭头函数,参数就是valid ,返回布尔值,通过这个布尔值来判断是否验证通过
+      -->
+      <el-form
+        :model="userForm"
+        :rules="addUserRules"
+        ref="addUserForm"
+        >
+        <el-form-item label="用户名" label-width="100px" prop="username">
+          <el-input v-model="userForm.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" label-width="100px" prop="password">
+          <el-input type="password" v-model="userForm.password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" label-width="100px" prop="email">
+          <el-input v-model="userForm.email" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" label-width="100px" prop="mobile">
+          <el-input v-model="userForm.mobile" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="handleAddUser">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -100,7 +136,33 @@ export default {
       tableData: [], // 表格列表数据
       totalSize: 0, // 总数据条数
       currentPage: 1, // 当前页码
-      pageSize: 3 // 每页条数
+      pageSize: 3, // 每页条数
+      userForm: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      },
+      dialogFormVisible: false,
+      // 添加表单验证规则
+      addUserRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 8, message: '长度在 3 到 8 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 12, message: '长度在 6 到 12 个字符', trigger: 'blur' }
+        ],
+        email: [
+          { required: true, message: '请输入邮箱地址', trigger: 'blur' },
+          { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur'] }
+        ],
+        mobile: [
+          { required: true, message: '请输入电话', trigger: 'blur' },
+          { min: 7, max: 11, message: '长度在 7 到 11 个字符', trigger: 'blur' }
+        ]
+      }
     }
   },
   methods: {
@@ -123,7 +185,7 @@ export default {
       this.loadUsersByPage(currentPage)
     },
     // 根据页码加载用户数据
-    async loadUsersByPage (page, pageSize = 1) {
+    async loadUsersByPage (page) { // 如果用户没有指定条数,默认1条每页
       const res = await this.$http.get('/users', {
         params: { // 配置参数 pagenum 和pagesize 不能为空
           pagenum: page, // 这个页码就是当前页码:current-page
@@ -140,6 +202,39 @@ export default {
     // 处理搜索功能
     handleSearch () {
       this.loadUsersByPage(1) // 从第一页开始展示
+    },
+    // 点击确定添加用户
+    async handleAddUser () {
+      // 获取用户信息
+      // console.log(this.userForm)
+      // 表单验证
+      // 发起请求,添加用户
+      // 如果成功,关闭对话框,重新加载最新列表数据
+
+      // VUE的$refs 方法可以用来获取设置了ref属性的DOM
+      // console.log(this.$refs['addUserForm'])
+
+      // 调用validate()方法 强行验证表单,参数 valid 就是布尔值
+      this.$refs['addUserForm'].validate(async (valid) => {
+        // console.log(vaild)
+        if (!valid) { // 如果验证失败,返回 不提交
+          return false
+        }
+        // 如果通过了表单验证
+        const res = await this.$http.post('/users', this.userForm)
+        // console.log(res)
+        if (res.data.meta.status === 201) {
+          // 根据响应做交互
+          this.$message({
+            type: 'success',
+            message: '添加用户成功!'
+          })
+          // 关闭对话框
+          this.dialogFormVisible = false
+          // 加载当前页
+          this.loadUsersByPage(this.currentPage)
+        }
+      })
     },
     // switch开关状态改变,修改用户的登录状态
     async handleStateChange (state, user) {
