5e5cbece031bb34ffb01f49ec38fd2f9580131fe
Tatiana <tatiana520@163.com>
2018-11-16 16:37:17 +0800

    :sparkles:抽取部分全局css样式&渲染权限列表&增加loading功能

--- src/components/rights-list/script.js
+++ src/components/rights-list/script.js
@@ -1,5 +1,24 @@
 export default {
+  created () {
+    this.loadRightsList()
+  },
   data () {
-    return {}
+    return {
+      tableData: [
+
+      ],
+      loading: true
+    }
+  },
+  methods: {
+    async loadRightsList () {
+      const res = await this.$http.get('./rights/list')
+      // console.log(res)
+      if (res.data.meta.status === 200) {
+        this.tableData = res.data.data
+        // 数据加载完毕,让loading隐藏
+        this.loading = false
+      }
+    }
   }
 }
