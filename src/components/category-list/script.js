a528915ee7d9f4246d0d309208bb9bb480eb0dff
Tatiana <tatiana520@163.com>
2018-11-23 17:47:55 +0800

    :zap:添加分类の展示级联选择器分类数据

--- src/components/category-list/script.js
+++ src/components/category-list/script.js
@@ -8,7 +8,12 @@ export default {
       currentPage: 1,
       pageSize: 10,
       totalSize: 0,
-      loading: true
+      loading: true,
+      addCateDia: false,
+      addCateForm: {
+      },
+      cateOptions: [],
+      selectedOptions: []
     }
   },
   methods: {
@@ -29,6 +34,10 @@ export default {
         this.loading = false // 取消loading效果
       }
     },
+    // 级联选择器改变的事件处理函数
+    handleCascadeChange (val) {
+      console.log(val)
+    },
     handleSizeChange (pageSize) {
       this.pageSize = pageSize
       this.loadCategories(1)
@@ -36,6 +45,22 @@ export default {
     },
     handleCurrentChange (page ) {
       this.loadCategories(page)
+    },
+    // 显示添加分类对话框
+    async handleShowCate () {
+      // 加载二级分类列表数据到级联选择器中
+      const res = await this.$http.get('/categories',{
+        params: {
+          type: 2
+        }
+      })
+      // console.log(res)
+      const {data, meta} = res.data
+      if (meta.status === 200) {
+        // 显示弹框
+        this.addCateDia = true
+        this.cateOptions = data
+      }
     }
   }
 }
