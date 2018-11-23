c4e05ea055ccf37ec02321ec8c94f294cdd36fe7
Tatiana <tatiana520@163.com>
2018-11-23 15:58:15 +0800

    :art:完成商品分类动态分页展示

--- src/components/category-list/script.js
+++ src/components/category-list/script.js
@@ -1,35 +1,41 @@
 export default {
-  created () {},
+  created () {
+    this.loadCategories(1)
+  },
   data () {
     return {
-      tableDate: [
-        {
-          "cat_id": 1,
-          "cat_name": "大家电",
-          "cat_pid": 0,
-          "cat_level": 0,
-          "cat_deleted": false,
-          "children": [
-            {
-              "cat_id": 2,
-              "cat_name": "电视",
-              "cat_pid": 1,
-              "cat_level": 1,
-              "cat_deleted": false,
-              "children": [
-                {
-                "cat_id": 3,
-                "cat_name": "曲面电视",
-                "cat_pid": 2,
-                "cat_level": 2,
-                "cat_deleted": false
-                }
-              ]
-            }
-          ]
-        }
-      ]
+      tableDate: [],
+      currentPage: 1,
+      pageSize: 10,
+      totalSize: 0,
+      loading: true
     }
   },
-  methods: {}
+  methods: {
+    async loadCategories (page) {
+      this.loading = true
+      const res = await this.$http.get('/categories',{
+        params: {
+          type: 3,
+          pagenum: page,
+          pagesize: this.pageSize
+        }
+      })
+      // console.log(res)
+      const {data, meta} = res.data
+      if (meta.status === 200) {
+        this.tableDate = data.result
+        this.totalSize = data.total
+        this.loading = false // 取消loading效果
+      }
+    },
+    handleSizeChange (pageSize) {
+      this.pageSize = pageSize
+      this.loadCategories(1)
+      this.currentPage = 1 // 让页面高亮效果回归到第一页
+    },
+    handleCurrentChange (page ) {
+      this.loadCategories(page)
+    }
+  }
 }
