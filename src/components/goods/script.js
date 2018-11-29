87e66cbb1e78fc91712898b3341196e613cfe6f1
Tatiana <tatiana520@163.com>
2018-11-29 16:00:41 +0800

    完成商品管理商品列表页面布局&商品数据动态展示

--- src/components/goods/script.js
+++ src/components/goods/script.js
@@ -1,7 +1,46 @@
 export default {
-  created () {},
-  data () {
-    return {}
+  created () {
+    this.loadGoods(1)
   },
-  methods: {}
+  data () {
+    return {
+      tableData: [
+        {
+          goods_name: '',
+          goods_price: '',
+          goods_state: '',
+          goods_weight: '',
+          add_time: '',
+        }
+      ],
+      pagenum: 1,
+      pagesize: 10,
+      currentPage: 1,
+      totalSize: 0
+    }
+  },
+  methods: {
+    async loadGoods (page) {
+      const res = await this.$http.get(`/goods`, {
+        params: {
+          pagenum: page,
+          pagesize: this.pagesize
+        }
+      })
+      const {data, meta} = res.data
+      // console.log(data)
+      if (meta.status === 200) {
+        this.totalSize = data.total
+        this.tableData = data.goods
+      }
+    },
+    handleSizeChange (val) {
+      this.pageSiza = val
+      this.loadGoods(1)
+      this.currentPage = 1 // 页码高亮回归第一页
+    },
+    handleCurrentChange (val) {
+      this.loadGoods(val)
+    }
+  }
 }
